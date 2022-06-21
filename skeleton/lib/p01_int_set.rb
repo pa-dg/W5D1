011c9c14eebc2d44d4abd955e50ca6e1b2912fc2
pdeguzman <padgzmn@gmail.com>
2022-06-20 23:36:03 -0400

    added W5D1 project and completed phase one

---
+++ skeleton/lib/p01_int_set.rb
@@ -0,0 +1,90 @@
+class MaxIntSet
+  attr_reader :max, :store
+  def initialize(max)
+    @max = max
+    @store = Array.new(max, false)
+  end
+
+  def insert(num)
+    raise "Out of bounds" if !num.between?(0, self.max)
+
+    self.store[num] = true
+  end
+
+  def remove(num)
+    self.store[num] = false
+  end
+
+  def include?(num)
+    return self.store[num]
+  end
+
+  private
+
+  def is_valid?(num)
+  end
+
+  def validate!(num)
+  end
+end
+
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+    result = num % self.store.length
+    self.store[num] = result
+    
+  end
+
+  def remove(num)
+  end
+
+  def include?(num)
+
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+  end
+
+  def remove(num)
+  end
+
+  def include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+  end
+end
